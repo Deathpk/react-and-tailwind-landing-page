32a7998bf053c7578d5e74355b323dedd5213882
versianidev <bettercallmiguel@gmail.com>
2022-11-04 18:02:40 -0300

    New - All Components created.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from "react";
 import Analytics from "./components/Analytics";
+import Cards from "./components/Cards";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import NewsLetter from "./components/NewsLetter";
 
 function App() {
   return (
     <div>
         <Navbar />
-        <Hero/>
-        <Analytics/>
+        <Hero />
+        <Analytics />
+        <NewsLetter />
+        <Cards />
+        <Footer />
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,9 +18,9 @@ const Navbar = () => {
                 <li className="p-4">Contact</li>
             </ul>
             <div onClick={handleMobileMenu} className='block md:hidden'>
-                {mobileMenuIsClosed ? <AiOutlineMenu size={20} /> : <AiOutlineClose size={20} /> }
+                {mobileMenuIsClosed ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} /> }
             </div>
-            <div className= {mobileMenuIsClosed ? 'fixed left-[-100%] ease-out duration-500' : 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500'}>
+            <div className= {!mobileMenuIsClosed ? 'fixed left-[-100%] ease-out duration-500' : 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500'}>
             <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">React.</h1>
                 <ul className="uppercase p-4">
                 <li className="p-4 border-b border-gray-600">Home</li>
